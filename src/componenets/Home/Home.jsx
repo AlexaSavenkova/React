c4c43426789460b9dab55c8c107c1159f1b478e2
Alexandra <sav_alexandra@yahoo.com>
2022-07-02 12:37:16 +0200

    homework for lesson 5

--- src/componenets/Home/Home.jsx
+++ src/componenets/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 const Home = () => {
+    const count = useSelector(state => state.count);
+    const dispatch = useDispatch();
+
     return (
-        <h3>
-            This is a home page
-        </h3>
+        <div style={{padding: '15px'}}>
+            <h3>
+                This is a home page
+            </h3>
+            <h4>Счетчик</h4>
+
+            <button onClick={() => dispatch({type: 'minus'})}>-</button>
+                &nbsp;
+                {count}
+                &nbsp;
+            <button onClick={() => dispatch({type: 'plus'})}>+</button>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './componenets/App/App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+import App from './componenets/App/App';
+
+import './index.css';
+
+
+const initialState = {
+    count: 0,
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'plus':
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case 'minus':
+            return {
+                ...state,
+                count: state.count -1
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
-    </BrowserRouter>
+    <Provider store = {store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
